85dfda66b6dcc27a89f78aad130f71f16570e4ce
Martin Lazarov <martin@example.com>
2022-01-22 14:14:50 +0200

    fix: remove code and packages

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -5,12 +5,12 @@ const winston = require("winston");
 const level = process.env.LOG_LEVEL || 'debug';
 
 export const logger = new winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            level: level,
-            timestamp: function () {
-                return moment().format('YYYY-MM-DD hh:mm:ss').trim();
-            }
-        })
-    ]
+  transports: [
+    new winston.transports.Console({
+      level: level,
+      timestamp: function () {
+        return moment().format('YYYY-MM-DD hh:mm:ss').trim();
+      }
+    })
+  ]
 });
